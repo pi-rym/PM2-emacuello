deb7948364e1026ffbe7367478d5eda127506204
Emanuel Cuello <ema.cuello1010@gmail.com>
2024-03-11 17:32:44 -0300

    creacion de func asinc y axios

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,8 +1,18 @@
 const crearCards = require('./renderCards.js')
 
-$.get('https://students-api.2.us-1.fl0.io/movies', (data) => {
-    data.forEach(crearCards)
-})
+const axios = require('axios')
+
+const datos = async(url) => {
+    try {
+        const data = await axios(url);
+        const cards = data.data;
+        cards.forEach(crearCards); 
+    } catch (error) {
+        error.message = 'Error de API'
+    }
+}
+
+datos('https://students-api.2.us-1.fl0.io/movies');
 
 
 

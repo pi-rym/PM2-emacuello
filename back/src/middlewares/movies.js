766a6f8485aa09f31210f9ce55147bab4d5890a1
Emanuel Cuello <ema.cuello1010@gmail.com>
2024-03-20 16:51:47 -0300

    agregando middleware de validacion

--- back/src/middlewares/movies.js
+++ back/src/middlewares/movies.js
@@ -1,3 +1,7 @@
 const validateMovie = (req, res, next) => {
-    
+    const {title, year, director,duration, genre, rate, poster} = req.body;
+    if (![ title, year, director,duration, genre, rate, poster].every(Boolean)) return res.status(400).json({message: "Los datos estan incompletos"});
+    next()
 }
+
+module.exports = validateMovie;
--- back/src/routes/moviesRoutes.js
+++ back/src/routes/moviesRoutes.js
@@ -1,10 +1,10 @@
 const {Router} = require('express')
 const moviesRoutes = Router()
 const {getPeliculas, postMovies, sendMovies} = require('../controllers/moviesControllers')
-
+const validateMovie = require('../middlewares/movies')
 
 moviesRoutes.get('/movies',getPeliculas)
 moviesRoutes.get('/movies/:id', sendMovies)
-moviesRoutes.post('/movies',postMovies)
+moviesRoutes.post('/movies',validateMovie ,postMovies)
 
 module.exports = moviesRoutes
